c75ca765c69d1e19a5a24f7c1fca81f9d424bb3b
Disha13raghu <171815730+Disha13raghu@users.noreply.github.com>
2024-12-05 00:12:23 +0530

    Update try.py

--- Hand_written_digit_recognizer_ml_application/try.py
+++ Hand_written_digit_recognizer_ml_application/try.py
@@ -1,19 +1,19 @@
-import tensorflow as tf
+iimport tensorflow as tf
 import numpy as np
 
-# Load MNIST dataset
+
 mnist = tf.keras.datasets.mnist
 (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-# Normalize the images to [0, 1]
+
+
 X_train = X_train / 255.0
 X_test = X_test / 255.0
 
-# Reshape the data to have the shape (28, 28, 1)
 X_train = X_train.reshape(-1, 28, 28, 1)
 X_test = X_test.reshape(-1, 28, 28, 1)
 
-# Define the model
+
 model = tf.keras.models.Sequential([
     tf.keras.layers.Conv2D(32, (3, 3), activation='relu', input_shape=(28, 28, 1)),
     tf.keras.layers.MaxPooling2D((2, 2)),
@@ -21,14 +21,14 @@ model = tf.keras.models.Sequential([
     tf.keras.layers.MaxPooling2D((2, 2)),
     tf.keras.layers.Flatten(),
     tf.keras.layers.Dense(64, activation='relu'),
-    tf.keras.layers.Dense(10, activation='softmax')  # Use softmax for multi-class classification
+    tf.keras.layers.Dense(10, activation='softmax')  
 ])
 
-# Compile the model
+
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model
-model.fit(X_train, y_train, epochs=3)
 
-# Save the model
+model.fit(X_train, y_train, epochs=20)
+
+
 model.save('digits2.keras')
